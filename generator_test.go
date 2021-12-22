fc91ebf1177b577231cc02081bf4e6722ff14b53
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2021-12-22 13:12:46 +0800

    update TestGenerateExecute, add TestGenerateReverseTCP.

--- generator_test.go
+++ generator_test.go
@@ -8,47 +8,88 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGenerateExecuteClass(t *testing.T) {
+func TestGenerateExecute(t *testing.T) {
 	template, err := os.ReadFile("testdata/template/Exec.class")
 	require.NoError(t, err)
 	spew.Dump(template)
 
 	t.Run("common", func(t *testing.T) {
-		class, err := GenerateExecuteClass(template, "whoami", "Test")
+		class, err := GenerateExecute(template, "whoami", "Test")
 		require.NoError(t, err)
 		spew.Dump(class)
 	})
 
 	t.Run("default class", func(t *testing.T) {
-		class, err := GenerateExecuteClass(template, "${cmd}", "")
+		class, err := GenerateExecute(template, "${cmd}", "")
 		require.NoError(t, err)
 		spew.Dump(class)
+
 		require.Equal(t, template, class)
 	})
 
 	t.Run("compare with Calc", func(t *testing.T) {
-		class, err := GenerateExecuteClass(template, "calc", "Calc")
+		class, err := GenerateExecute(template, "calc", "Calc")
 		require.NoError(t, err)
 		spew.Dump(class)
 
-		expected, err := os.ReadFile("testdata/template/Calc.class")
+		expected, err := os.ReadFile("testdata/template/compare/Calc.class")
 		require.NoError(t, err)
 		require.Equal(t, expected, class)
 	})
 
 	t.Run("compare with Notepad", func(t *testing.T) {
-		class, err := GenerateExecuteClass(template, "notepad", "Notepad")
+		class, err := GenerateExecute(template, "notepad", "Notepad")
 		require.NoError(t, err)
 		spew.Dump(class)
 
-		expected, err := os.ReadFile("testdata/template/Notepad.class")
+		expected, err := os.ReadFile("testdata/template/compare/Notepad.class")
 		require.NoError(t, err)
 		require.Equal(t, expected, class)
 	})
 
 	t.Run("empty command", func(t *testing.T) {
-		class, err := GenerateExecuteClass(template, "", "Test")
+		class, err := GenerateExecute(template, "", "Test")
 		require.EqualError(t, err, "empty command")
+		require.Zero(t, class)
+	})
+}
+
+func TestGenerateReverseTCP(t *testing.T) {
+	template, err := os.ReadFile("testdata/template/ReverseTCP.class")
+	require.NoError(t, err)
+	spew.Dump(template)
+
+	t.Run("common", func(t *testing.T) {
+		class, err := GenerateReverseTCP(template, "127.0.0.1", 9979, "", "Test")
+		require.NoError(t, err)
+		spew.Dump(class)
+	})
+
+	t.Run("default class", func(t *testing.T) {
+		class, err := GenerateReverseTCP(template, "127.0.0.1", 9979, "test", "")
+		require.NoError(t, err)
+		spew.Dump(class)
+	})
+
+	t.Run("compare", func(t *testing.T) {
+		class, err := GenerateReverseTCP(template, "127.0.0.1", 9979, "test", "ReTCP")
+		require.NoError(t, err)
+		spew.Dump(class)
+
+		expected, err := os.ReadFile("testdata/template/compare/ReTCP.class")
+		require.NoError(t, err)
+		require.Equal(t, expected, class)
+	})
+
+	t.Run("empty host", func(t *testing.T) {
+		class, err := GenerateReverseTCP(template, "", 1234, "", "")
+		require.EqualError(t, err, "empty host")
+		require.Zero(t, class)
+	})
+
+	t.Run("zero port", func(t *testing.T) {
+		class, err := GenerateReverseTCP(template, "127.0.0.1", 0, "", "")
+		require.EqualError(t, err, "zero port")
 		require.Zero(t, class)
 	})
 }
