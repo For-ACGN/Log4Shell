5d07dd4ca7286f653fbf7549ddbd1ffd1e3138ae
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2021-12-23 16:04:20 +0800

    add TestGenerateReverseHTTPS_Fake for pass TestGenerateReverseHTTPS.

--- generator_test.go
+++ generator_test.go
@@ -231,11 +231,11 @@ func TestGenerateReverseHTTPS(t *testing.T) {
 func TestGenerateReverseTCP_Fake(t *testing.T) {
 	t.Run("template", func(t *testing.T) {
 		const (
-			fileNameFlag = "ReverseTCP.java"
-			hostFlag     = "${host}"
-			portFlag     = "${port}"
-			tokenFlag    = "${token}"
-			className    = "ReverseTCP\x0C"
+			fileName  = "ReverseTCP.java"
+			hostFlag  = "${host}"
+			portFlag  = "${port}"
+			tokenFlag = "${token}"
+			className = "ReverseTCP\x0C"
 		)
 
 		buf := bytes.NewBuffer(make([]byte, 0, 128))
@@ -244,9 +244,9 @@ func TestGenerateReverseTCP_Fake(t *testing.T) {
 
 		size := make([]byte, 2)
 
-		binary.BigEndian.PutUint16(size, uint16(len(fileNameFlag)))
+		binary.BigEndian.PutUint16(size, uint16(len(fileName)))
 		buf.Write(size)
-		buf.WriteString(fileNameFlag)
+		buf.WriteString(fileName)
 		buf.Write([]byte{0x00, 0x00})
 
 		binary.BigEndian.PutUint16(size, uint16(len(hostFlag)))
@@ -275,11 +275,11 @@ func TestGenerateReverseTCP_Fake(t *testing.T) {
 
 	t.Run("compare", func(t *testing.T) {
 		const (
-			fileNameFlag = "ReTCP.java"
-			hostFlag     = "127.0.0.1"
-			portFlag     = "9979"
-			tokenFlag    = "test"
-			className    = "ReTCP\x0C"
+			fileName  = "ReTCP.java"
+			host      = "127.0.0.1"
+			port      = "9979"
+			token     = "test"
+			className = "ReTCP\x0C"
 		)
 
 		buf := bytes.NewBuffer(make([]byte, 0, 128))
@@ -288,24 +288,24 @@ func TestGenerateReverseTCP_Fake(t *testing.T) {
 
 		size := make([]byte, 2)
 
-		binary.BigEndian.PutUint16(size, uint16(len(fileNameFlag)))
+		binary.BigEndian.PutUint16(size, uint16(len(fileName)))
 		buf.Write(size)
-		buf.WriteString(fileNameFlag)
+		buf.WriteString(fileName)
 		buf.Write([]byte{0x00, 0x00})
 
-		binary.BigEndian.PutUint16(size, uint16(len(hostFlag)))
+		binary.BigEndian.PutUint16(size, uint16(len(host)))
 		buf.Write(size)
-		buf.WriteString(hostFlag)
+		buf.WriteString(host)
 		buf.Write([]byte{0x00, 0x00})
 
-		binary.BigEndian.PutUint16(size, uint16(len(portFlag)))
+		binary.BigEndian.PutUint16(size, uint16(len(port)))
 		buf.Write(size)
-		buf.WriteString(portFlag)
+		buf.WriteString(port)
 		buf.Write([]byte{0x00, 0x00})
 
-		binary.BigEndian.PutUint16(size, uint16(len(tokenFlag)))
+		binary.BigEndian.PutUint16(size, uint16(len(token)))
 		buf.Write(size)
-		buf.WriteString(tokenFlag)
+		buf.WriteString(token)
 		buf.Write([]byte{0x00, 0x00})
 
 		binary.BigEndian.PutUint16(size, uint16(len(className)-1))
@@ -314,6 +314,99 @@ func TestGenerateReverseTCP_Fake(t *testing.T) {
 		buf.Write([]byte{0x00, 0x00})
 
 		err := os.WriteFile("testdata/template/compare/ReTCP.class", buf.Bytes(), 0600)
+		require.NoError(t, err)
+	})
+}
+
+func TestGenerateReverseHTTPS_Fake(t *testing.T) {
+	t.Run("template", func(t *testing.T) {
+		const (
+			fileName  = "ReverseHTTPS.java"
+			urlFlag   = "${url}"
+			uaFlag    = "${ua}"
+			tokenFlag = "${token}"
+			className = "ReverseHTTPS\x0C"
+		)
+
+		buf := bytes.NewBuffer(make([]byte, 0, 128))
+		buf.Write([]byte{0xCA, 0xFE})
+		buf.Write([]byte{0x00, 0x00})
+
+		size := make([]byte, 2)
+
+		binary.BigEndian.PutUint16(size, uint16(len(fileName)))
+		buf.Write(size)
+		buf.WriteString(fileName)
+		buf.Write([]byte{0x00, 0x00})
+
+		binary.BigEndian.PutUint16(size, uint16(len(urlFlag)))
+		buf.Write(size)
+		buf.WriteString(urlFlag)
+		buf.Write([]byte{0x00, 0x00})
+
+		binary.BigEndian.PutUint16(size, uint16(len(uaFlag)))
+		buf.Write(size)
+		buf.WriteString(uaFlag)
+		buf.Write([]byte{0x00, 0x00})
+
+		binary.BigEndian.PutUint16(size, uint16(len(tokenFlag)))
+		buf.Write(size)
+		buf.WriteString(tokenFlag)
+		buf.Write([]byte{0x00, 0x00})
+
+		binary.BigEndian.PutUint16(size, uint16(len(className)))
+		buf.Write(size)
+		buf.WriteString(className)
+		buf.Write([]byte{0x00, 0x00})
+
+		err := os.WriteFile("testdata/template/ReverseHTTPS.class", buf.Bytes(), 0600)
+		require.NoError(t, err)
+	})
+
+	t.Run("compare", func(t *testing.T) {
+		const (
+			fileName = "ReHTTPS.java"
+			url      = "https://127.0.0.1:8443/test/" +
+				"0YjdeS7_m93CecZoo8Ntkgs8lRd8_P50Ud2378Ggsvu0FX3VfHF2jbRAQxfUk1Uklj" +
+				"sZ0Pwz-_bPfTMmytR-fhVGYvyEm-bPNat3i0XRJnm5oH76MBegc7AG3hEe1J1WG3PD" +
+				"vddN5Id06qqBQR9lZAkJNzFB6VPRJmbsvp_LKp3JDg70FrOcjczkGSRbeht14__lN"
+			ua        = "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko"
+			token     = "test"
+			className = "ReHTTPS\x0C"
+		)
+
+		buf := bytes.NewBuffer(make([]byte, 0, 128))
+		buf.Write([]byte{0xCA, 0xFE})
+		buf.Write([]byte{0x00, 0x00})
+
+		size := make([]byte, 2)
+
+		binary.BigEndian.PutUint16(size, uint16(len(fileName)))
+		buf.Write(size)
+		buf.WriteString(fileName)
+		buf.Write([]byte{0x00, 0x00})
+
+		binary.BigEndian.PutUint16(size, uint16(len(url)))
+		buf.Write(size)
+		buf.WriteString(url)
+		buf.Write([]byte{0x00, 0x00})
+
+		binary.BigEndian.PutUint16(size, uint16(len(ua)))
+		buf.Write(size)
+		buf.WriteString(ua)
+		buf.Write([]byte{0x00, 0x00})
+
+		binary.BigEndian.PutUint16(size, uint16(len(token)))
+		buf.Write(size)
+		buf.WriteString(token)
+		buf.Write([]byte{0x00, 0x00})
+
+		binary.BigEndian.PutUint16(size, uint16(len(className)-1))
+		buf.Write(size)
+		buf.WriteString(className)
+		buf.Write([]byte{0x00, 0x00})
+
+		err := os.WriteFile("testdata/template/compare/ReHTTPS.class", buf.Bytes(), 0600)
 		require.NoError(t, err)
 	})
 }
